aaf2fe3683d91d1e80c40c270e3ba48bb1bed1ad
ddkogit <git config --global user.email>
2024-02-27 10:49:15 +0545

    Destination page content rendered using state for that page

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-
-function Destinations({destinations}) {
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Destinations.css";
+function Destinations({ destinations }) {
   console.log(destinations);
+
+  const [currentDest, setCurrentDest]= useState(destinations[0]);
+
+  const handleClick=(dest)=>{
+      setCurrentDest(dest);
+  }
+
   return (
-    <div>Destinations</div>
-  )
+    <>
+      <div>01 PICK YOUR DESTINATION</div>
+
+      <div>
+        <nav className="dest-nav">
+          {destinations.map((dest) => (
+            <NavLink 
+            onClick={()=>handleClick(dest)}
+              to={`/destinations`}
+              activeClassName="activeLink"
+              className="alink"
+            >
+              {dest.name}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+      <div className="content">
+        {currentDest.name}
+        {currentDest.description}
+      </div>
+    </>
+  );
 }
 
-export default Destinations
+export default Destinations;
