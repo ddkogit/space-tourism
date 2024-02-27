7165e620ae7b3e32245564f1c11063ea87f2fa4c
ddkogit <git config --global user.email>
2024-02-27 11:06:30 +0545

    Add data fetched in all pages

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-function Crew({crew}) {
-  console.log(crew)
+function Crew({ crew }) {
+  const [currentCrew, setCurrentCrew] = useState(crew[0]);
+  const handleClick = (crew) => {
+    setCurrentCrew(crew);
+  };
+
   return (
-    <div>Crew</div>
-  )
+    <div>
+      {crew.map((item) => (
+        <NavLink onClick={() => handleClick(item)} to="/crew" activeClassName="activelink" className="crewlink">
+          0
+        </NavLink>
+      ))}
+      <div>{currentCrew.name}</div>
+    </div>
+  );
 }
 
-export default Crew
+export default Crew;
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function Technology({ technology }) {
-  console.log(technology);
-  return <div>Technology</div>;
+
+  const [currentTech,setCurrentTech]= useState(technology[0]);
+  const handleClick =(tech)=>{
+    setCurrentTech(tech);
+  }
+
+
+  return <div>
+    <nav>
+      {technology.map((tech,index)=>(
+        <NavLink activeClassName="activelink" className="techlink" onClick={()=>handleClick(tech)}>
+          {index+1}
+        </NavLink>
+      ))}
+    </nav>
+    <div>
+      {currentTech.name}
+    </div>
+  </div>;
 }
 
 export default Technology;
