ad951597ba11c5c6c6a164cc7865ac62a7af1113
ddkogit <git config --global user.email>
2024-02-27 20:27:42 +0545

    destinaion design done:

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Destinations.css";
 
-function Destinations({ destinations,background,setBg }) {
+function Destinations({ destinations, background, setBg }) {
   const [currentDest, setCurrentDest] = useState(destinations[0]);
   console.log(currentDest.images.png);
 
@@ -10,34 +10,51 @@ function Destinations({ destinations,background,setBg }) {
     setCurrentDest(dest);
   };
 
-  useEffect(()=>{
-    setBg (background)
-  },[]);
-
+  useEffect(() => {
+    setBg(background);
+  }, []);
 
   return (
     <>
-      <div>01 PICK YOUR DESTINATION</div>
+      <div className="containerdest">
+        <div className="leftdest">
+          <div className="h5 desttitle">01 PICK YOUR DESTINATION</div>{" "}
+          <img
+            className="imgdest"
+            src={currentDest.images.png}
+            alt={currentDest.name}
+          />
+        </div>
 
-      <div>
-        <nav className="dest-nav">
-          {destinations.map((dest) => (
-            <NavLink
-              onClick={() => handleClick(dest)}
-              to={`/destinations`}
-              activeClassName="activeLink"
-              className="alink"
-            >
-              {dest.name}
-            </NavLink>
-          ))}
-        </nav>
-      </div>
-      <div className="content">
-        {currentDest.name}
-        {currentDest.description}
+        <div className="rightdest">
+          <nav className="dest-nav navtext">
+            {destinations.map((dest) => (
+              <NavLink
+                onClick={() => handleClick(dest)}
+                to="#"
+                className="heading1 alink"
+                
+              >
+                {dest.name}
+              </NavLink>
+            ))}
+          </nav>
+          <div className="content">
+            <div className="h2">{currentDest.name}</div>
+            <div className="desc">{currentDest.description}</div>
 
-        <img src={currentDest.images.png} alt={currentDest.name} />
+            <div className="info">
+              <div>
+                <p className="subheading2">AVG. DISTANCE</p>
+                <p className="subheading1">{currentDest.distance}</p>
+              </div>
+              <div>
+                <p className="subheading2">EST. TRAVEL TIME</p>
+                <p className="subheading1">{currentDest.travel}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
